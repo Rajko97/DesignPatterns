26b5b5f893299cd67c4ed41961f4ab4f2af0e8e0
Rajko <rmilanrajkovic@gmail.com>
2021-04-01 14:28:20 +0200

    factory example

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,15 @@
+const val noCurrencyCode = "Currency Code Available"
+
 fun main() {
-    println("Hello world!")
+    val currencyFactory = CurrencyFactory()
+
+    val greece = currencyFactory.make(Country.Greece)
+    val spain = currencyFactory.make(Country.Spain)
+    val unitedStates = currencyFactory.make(Country.UnitedStates)
+    val unitedKingdom = currencyFactory.make(Country.UK)
+
+    println(greece?.code() ?: noCurrencyCode)
+    println(spain?.code() ?: noCurrencyCode)
+    println(unitedStates?.code() ?: noCurrencyCode)
+    println(unitedKingdom?.code() ?: noCurrencyCode)
 }
