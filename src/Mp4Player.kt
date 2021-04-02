09153efd07907d488fd17084c52fcdab6c4ba55c
Rajko <rmilanrajkovic@gmail.com>
2021-04-02 10:50:48 +0200

    adapter example

---
+++ src/Mp4Player.kt
@@ -0,0 +1,9 @@
+class Mp4Player : AdvancedMediaPlayer {
+    override fun playVlc(fileName: String?) {
+        //do nothing
+    }
+
+    override fun playMp4(fileName: String?) {
+        println("Playing mp4 file. Name: $fileName")
+    }
+}
---
+++ src/VlcPlayer.kt
@@ -0,0 +1,9 @@
+class VlcPlayer : AdvancedMediaPlayer {
+    override fun playVlc(fileName: String?) {
+        println("Playing vlc file. Name: $fileName")
+    }
+
+    override fun playMp4(fileName: String?) {
+        //do nothing
+    }
+}
